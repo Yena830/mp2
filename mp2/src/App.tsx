d9484f5e3008f96b2bd574a199f4b22374e0191a
yena830 <lyy0256@outlook.com>
2025-10-01 03:24:30 -0500

    fix

--- mp2/src/App.tsx
+++ mp2/src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/mp2-yueyue4">
+    <Router basename="/mp2">
       <div className="App">
         <Routes>
           <Route path="/" element={<MainView />} />
